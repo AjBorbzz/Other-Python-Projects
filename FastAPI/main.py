c16ffe37534377b43e6cccd233f95a65beff3216
AjBorbzz <ajborbeheaddhz@yahoo.com>
2024-10-19 13:06:47 +0800

    Added FastAPI Enum.

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,7 +1,17 @@
+from enum import Enum
 from fastapi import FastAPI
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 app = FastAPI()
 
-@app.get("/items/{item_id}")
-async def read_item(item_id):
-    return {"item_id": item_id}
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "Message": "Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "Message": "LeCNN all the images"}
+    return {"moel_name": model_name, "Message": "Have some residuals"}
