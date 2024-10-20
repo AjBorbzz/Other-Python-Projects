ce9ccde8afcbcbc8f1e6283a1c2c7409d18dfd2f
AjBorbzz <ajborbeheaddhz@yahoo.com>
2024-10-20 18:02:28 +0800

    Intro query params.

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -8,6 +8,8 @@ class ModelName(str, Enum):
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
@@ -15,3 +17,7 @@ async def get_model(model_name: ModelName):
     if model_name.value == "lenet":
         return {"model_name": model_name, "Message": "LeCNN all the images"}
     return {"moel_name": model_name, "Message": "Have some residuals"}
+
+@app.get("/items/")
+async def read_items(skip: int=0, limit: int= 10):
+    return fake_items_db[skip : skip + limit]
