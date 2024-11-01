80cb39996f112990ef2879f67128abeceb17a863
AjBorbzz <ajborbeheaddhz@yahoo.com>
2024-11-01 18:05:31 +0800

    Added size in read_items method.

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -66,11 +66,15 @@ async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
     return results
 
 @app.get("/items/{item_id}")
-async def read_items(item_id: Annotated[int, Path(title="The ID of the item to get", ge=1)],
-                     q: Annotated[str | None, Query(alias="item-query")] = None,
+async def read_items(item_id: Annotated[int, Path(title="The ID of the item to get", gt=0, le=1000)],
+                     q: str,
+                     size: Annotated[float, Query(gt=0, lt = 10.5)]
                     ):
     results = {"item_id": item_id}
     if q:
         results.update({"q": q})
+
+    if size:
+        results.update({"size": size})
     return results
 
