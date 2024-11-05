92b1af3f4a7da6f423915be0837f328062922b48
AjBorbzz <ajborbeheaddhz@yahoo.com>
2024-11-05 23:21:34 +0800

    added Field in Item model.

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -5,8 +5,10 @@ from pydantic import BaseModel, Field
 
 class Item(BaseModel):
     name: str
-    description: str | None = None
-    price: float
+    description: str | None = Field(
+        default=None, title="The description of the item", max_length=300
+    )
+    price: float = Field(gt=0, description="The price must be greater than zero")
     tax: float | None = None
 
 class User(BaseModel):
