70af8816b1fe48f08e9ddd35a351d042eb27e1c6
AjBorbzz <ajborbeheaddhz@yahoo.com>
2024-11-12 16:41:51 +0800

    Basic Dataframe creation.

--- crawl4ai/app.py
+++ crawl4ai/app.py
@@ -5,6 +5,7 @@ async def main():
     async with AsyncWebCrawler(verbose=True) as crawler:
         result = await crawler.arun(url="https://www.nbcnews.com/business")
         print(f"Basic crawl result: {result.markdown[:500]}")  # Print first 500 characters
+        print(result.media)
 
 
 if __name__ == "__main__":
