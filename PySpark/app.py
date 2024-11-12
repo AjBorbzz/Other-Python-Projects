f21679a1518c773f75d624fd7b5a812f4c23e2ab
AjBorbzz <ajborbeheaddhz@yahoo.com>
2024-11-12 16:43:38 +0800

    DF Show and print schema.

--- PySpark/app.py
+++ PySpark/app.py
@@ -11,4 +11,14 @@ df = spark.createDataFrame([
     Row(a=2, b=3., c='string2', d=date(2000, 2, 1), e=datetime(2000, 1, 2, 12, 0)),
     Row(a=4, b=5., c='string3', d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0))
 ])
+
+pandas_df = pd.DataFrame({
+    'a': [1, 2, 3],
+    'b': [2., 3., 4.],
+    'c': ['string1', 'string2', 'string3'],
+    'd': [date(2000, 1, 1), date(2000, 2, 1), date(2000, 3, 1)],
+    'e': [datetime(2000, 1, 1, 12, 0), datetime(2000, 1, 2, 12, 0), datetime(2000, 1, 3, 12, 0)]
+})
+df = spark.createDataFrame(pandas_df)
 print(df)
+df.show()
