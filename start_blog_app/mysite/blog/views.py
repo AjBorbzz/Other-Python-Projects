60e260cd1c40c619f238cf86c46ed9c20e23512a
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-02-21 00:11:58 +0800

    Added custom 404 page.

--- start_blog_app/mysite/blog/views.py
+++ start_blog_app/mysite/blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import Post
 from django.shortcuts import get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger, InvalidPage
 import markdown
 from django.views.generic import ListView
+from django.http import Http404
 
 
 class PostListView(ListView):
@@ -12,6 +13,42 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.xhtml'
+
+    def get_template_names(self):
+        if self.request.GET.get('page') and not self.request.GET.get('page').isdigit():
+            raise Http404
+        if getattr(self, 'page_not_found', False):
+            return ['404.html']
+        return [self.template_name]
+    
+    def paginate_queryset(self, queryset, page_size):
+        """
+        Paginate the queryset, and handle potential 404 errors.
+        """
+        paginator = self.get_paginator(
+            queryset, page_size, orphans=self.get_paginate_orphans(),
+            allow_empty_first_page=self.get_allow_empty())
+        page_kwarg = self.page_kwarg
+        page = self.kwargs.get(page_kwarg) or self.request.GET.get(page_kwarg) or 1
+        try:
+            page_number = int(page)
+        except ValueError:
+            if page == 'last':
+                page_number = paginator.num_pages
+            else:
+                # Raise a 404 for non-integer page numbers *before* EmptyPage
+                self.page_not_found = True  # Set the flag
+                return (paginator, None, [], False) # Return to avoid raising the original exception.
+                # raise Http404("Invalid page number.") # Better place for Http404
+
+        try:
+            page = paginator.page(page_number)
+            return (paginator, page, page.object_list, page.has_other_pages())
+        except InvalidPage as e:
+             # Set the flag for get_template_names()
+            self.page_not_found = True
+             #Return to avoid raising EmptyPage
+            return (paginator, None, [], False)
 
 def post_list(request):
     post_list = Post.published.all()
