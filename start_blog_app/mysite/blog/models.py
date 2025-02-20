4e40e740786290afe446c76fec645fa095b84d3e
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-02-20 23:43:12 +0800

    Added pagination and markdown for blog post.

--- start_blog_app/mysite/blog/models.py
+++ start_blog_app/mysite/blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset()\
@@ -13,6 +14,7 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
+    
     title= models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User,
@@ -31,9 +33,12 @@ class Post(models.Model):
     class Meta:
         ordering = ['-publish']
         indexes = [models.Index(fields=['-publish']),]
+        verbose_name_plural = "Markdown Content"
 
     def __str__(self):
         return self.title
+    
+    
     
     def get_absolute_url(self):
         return reverse('blog:post_detail',
--- start_blog_app/mysite/blog/views.py
+++ start_blog_app/mysite/blog/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import render
 from .models import Post
 from django.shortcuts import get_object_or_404
-
+from django.core.paginator import Paginator
+import markdown
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Pagination
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request,
                   'blog/post/list.xhtml',
                   {'posts': posts})
 
 def post_detail(request, year, month, day, post):
-    post= get_object_or_404(Post,
+    md = markdown.Markdown(extensions=["fenced_code"])
+    post = get_object_or_404(Post,
                             status=Post.Status.PUBLISHED,
                             slug=post,
                             publish__year=year,
                             publish__month=month,
                             publish__day=day
                             )
-    
+    post.body = md.convert(post.body)
     return render(request,
                   'blog/post/detail.xhtml',
                   {'post': post})
