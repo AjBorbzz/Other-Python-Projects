25f9368c30214abd54cd9ad4011074e902d466e2
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-02-21 21:52:20 +0800

    Added send_mail to views.py

--- start_blog_app/mysite/mysite/settings.py
+++ start_blog_app/mysite/mysite/settings.py
@@ -110,3 +110,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ajohnribanodev1@gmail.com'
+EMAIL_HOST_PASSWORD = 'fqzv gmre zydi hcdr'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
