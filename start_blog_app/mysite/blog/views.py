1253c9bb1b9a33590b9894bb140145b968ed5704
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-02-21 21:52:20 +0800

    Added send_mail to views.py

--- start_blog_app/mysite/blog/views.py
+++ start_blog_app/mysite/blog/views.py
@@ -6,16 +6,30 @@ import markdown
 from django.views.generic import ListView
 from django.http import Http404
 from .forms import EmailPostForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'ajohnribanodev1@gmail.com',
+                      [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.xhtml', {'post': post})
+    return render(request, 'blog/post/share.xhtml', {'post': post,
+                                                     'form': form,
+                                                     'sent': sent})
 
 
 class PostListView(ListView):
--- start_blog_app/mysite/mysite/settings.py
+++ start_blog_app/mysite/mysite/settings.py
@@ -110,3 +110,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ajohnribanodev1@gmail.com'
+EMAIL_HOST_PASSWORD = '***REMOVED-COMPROMISED-GMAIL-APP-PASSWORD***'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
