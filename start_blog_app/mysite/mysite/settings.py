323f16ca41720e521c174f878fe60ee779487feb
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-02-26 14:00:52 +0800

    Added full text search using SearchVector of Postgres.

--- start_blog_app/mysite/mysite/settings.py
+++ start_blog_app/mysite/mysite/settings.py
@@ -12,7 +12,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "taggit",
     "django.contrib.sites",
     "django.contrib.sitemaps",
+    "django.contrib.postgres"
 ]
 
 MIDDLEWARE = [
@@ -70,8 +71,10 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD")
     }
 }
 
