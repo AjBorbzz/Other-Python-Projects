46f2862dd0c20dcdbc754b15e4588b927ad2bcf9
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-04-09 23:09:21 +0800

    added extracting image

--- Read_PDF/extract_text.py
+++ Read_PDF/extract_text.py
@@ -5,6 +5,10 @@ out = open("output.txt", "wb") # create a text output
 
 for page in doc: # iterate the document pages
     text = page.get_text().encode("utf8") # get plain text (is in UTF-8)
+
+    tp = page.get_textpage_ocr()
+    text = page.get_text(textpage=tp)
+
     out.write(text) # write text of page
     out.write(bytes((12,))) # write page delimiter (form feed 0x0C)
 out.close()
