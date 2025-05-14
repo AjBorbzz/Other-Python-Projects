66a15bb84dd4bb3859c01961727bf528e39c4070
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-05-14 15:39:17 +0800

    added Marker to admin

--- DjangoProjects/DjangoGeoMap/mymap/markers/admin.py
+++ DjangoProjects/DjangoGeoMap/mymap/markers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from markers.models import Marker 
+
+
+@admin.register(Marker)
+class MarkerAdmin(admin.GISModelAdmin):
+    list_display = ("name", "location")
