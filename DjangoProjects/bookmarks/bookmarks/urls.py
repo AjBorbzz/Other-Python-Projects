a025077a39ef60d014119a56ec2cbfe8f9e13851
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-06-13 23:28:28 +0800

    Added media url and media root

--- DjangoProjects/bookmarks/bookmarks/urls.py
+++ DjangoProjects/bookmarks/bookmarks/urls.py
@@ -1,8 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static # serve media files
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
 ]
+
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    
+# NOTE: Never use the static() helper function in Production. Django is very inefficient at serving static files.
