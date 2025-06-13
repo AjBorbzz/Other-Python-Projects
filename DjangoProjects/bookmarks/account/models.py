9496ff50bfe2f983004bfe15dcc494faac95ae3b
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-06-13 23:28:28 +0800

    Added media url and media root

--- DjangoProjects/bookmarks/account/models.py
+++ DjangoProjects/bookmarks/account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE) # force the deletion of the related profile
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', # stores the relative path to the file in the related database field.
+                              blank=True)
+
+    def __str__(self):
+        return f'Profile of {self.user.username}'
