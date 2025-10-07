0a56ad42eca726bf355792616a319e5f150e1f31
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-10-07 16:21:21 +0800

    added format_arrival_time in utilities

--- SecurityAutomation/Python/utilities.py
+++ SecurityAutomation/Python/utilities.py
@@ -1,1 +1,8 @@
 """A collection of utilities for reuseable functions/methods."""
+from datetime import datetime, timezone, timedelta 
+
+def format_arrival_time(arrival_time: str) -> datetime:
+    """Formats the arrival time value from the log (SIEM)"""
+    dt = datetime.strptime(arrival_time.replace(" (UTC)", ""), "%d %b %Y %H:%M:%S.%f")
+    dt = dt.replace(tzinfo=timezone.utc)
+    return dt
