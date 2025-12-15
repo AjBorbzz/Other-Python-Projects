62a9b710a00c561e54e935bee0a8ffec9ff65d04
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-12-16 05:09:46 +0800

    my one time take sort algo

--- PyAdvance/data_structures_and_algorithm/sorting/insertion_sort.py
+++ PyAdvance/data_structures_and_algorithm/sorting/insertion_sort.py
@@ -2,7 +2,7 @@ def insertion_sort(arr):
     for i in range(1, len(arr)):
         key = arr[i]          # element to insert
         j = i - 1
-
+        
         # shift elements greater than key to the right
         while j >= 0 and arr[j] > key:
             arr[j + 1] = arr[j]
@@ -15,4 +15,15 @@ def insertion_sort(arr):
 
 
 data = [8, 3, 5, 2]
-print(insertion_sort(data))
+# print(insertion_sort(data))
+
+# another insertion code
+def insert_sort(arr, num_to_sort):
+    for i in range(num_to_sort+1):
+        if arr[num_to_sort-1] > arr[num_to_sort]:
+            arr[num_to_sort], arr[num_to_sort-1] = arr[num_to_sort-1], arr[num_to_sort]
+        else:
+            return arr
+        
+t = insert_sort([2,7,15,24,10,3,4,1,0], 4)
+print(t)
