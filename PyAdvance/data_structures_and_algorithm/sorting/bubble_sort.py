210ad469be74181d8e789fc08e123ac505cc4289
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-12-17 06:40:27 +0800

    Added an improved bubble sort

--- PyAdvance/data_structures_and_algorithm/sorting/bubble_sort.py
+++ PyAdvance/data_structures_and_algorithm/sorting/bubble_sort.py
@@ -10,3 +10,21 @@ def bubble_sort(arr):
 
 data = [5, 1, 4, 2, 8]
 print(bubble_sort(data))
+
+
+## An improvement
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        swapped = False
+
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+
+        if not swapped:
+            break
+
+    return arr
