1bea6286c5fe770a9c4fd740a891308cdc025e43
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-12-17 22:22:11 +0800

    Added very basic FastAPI CRUD

--- FastAPI/intro.py
+++ FastAPI/intro.py
@@ -11,6 +11,11 @@ class Task(BaseModel):
     description: Optional[str] = None
     completed: bool = False
 
+class TaskUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    completed: Optional[bool] = None
+
 task_db = []
 task_id_counter=1
 
@@ -23,3 +28,31 @@ def create_task(task: Task):
 
     task_db.append(task)
     return task
+
+@app.get("/tasks")
+def get_tasks():
+    return task_db
+
+@app.get("/tasks/{task_id}", response_model=Task)
+def get_task(task_id: int):
+    for task in task_db:
+        if task.id == task_id:
+            return task 
+        
+    raise HTTPException(status_code=404, detail="Task Not Found!")
+
+
+@app.put("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: int, task_update: TaskUpdate):
+    for task in task_db:
+        if task.id == task_id:
+            # Update only provided fields
+            if task_update.title is not None:
+                task.title = task_update.title
+            if task_update.description is not None:
+                task.description = task_update.description
+            if task_update.completed is not None:
+                task.completed = task_update.completed
+            return task
+    
+    raise HTTPException(status_code=404, detail="Task not found")
