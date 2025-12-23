f09d77e26021e28c0bad5ffcba79b4ce906112fb
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-12-24 02:23:34 +0800

    added login for token

--- FastAPI/CRUD.py
+++ FastAPI/CRUD.py
@@ -316,3 +316,11 @@ fake_users_db = {
 
 def fake_hash_password(password: str):
     return "fakehashed" + password
+
+
+@app.post("/token")
+async def login_for_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = fake_users_db.get(form_data.username)
+    if not user or fake_hash_password(form_data.password) != user["hashed_password"]:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    return {"access_token": user["username"], "token_type": "bearer"}
