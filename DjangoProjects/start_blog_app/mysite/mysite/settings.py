1a15f3ac5339130cb3b8a87d657b47c2b289b5d6
AjBorbzz <ajborbeheaddhz@yahoo.com>
2026-09-13 05:01:18 +0800

    Removed compromised cred

--- DjangoProjects/start_blog_app/mysite/mysite/settings.py
+++ DjangoProjects/start_blog_app/mysite/mysite/settings.py
@@ -125,11 +125,8 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ajohnribanodev1@gmail.com'
-EMAIL_HOST_PASSWORD = '***REMOVED-COMPROMISED-GMAIL-APP-PASSWORD***'
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-
-# Super user = djangotest
-# pw : taoako025
